5db06a5d0893c1ac6667336dd8657478f7cf1942
delinkmarket <delinkmarket@gmail.com>
2022-05-03 20:03:22 +0200

    first commit

---
+++ harika_react_spring/src/main/java/com/project/harika_react_spring/HarikaReactSpringApplication.java
@@ -0,0 +1,13 @@
+package com.project.harika_react_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarikaReactSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarikaReactSpringApplication.class, args);
+	}
+
+}
